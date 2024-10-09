71f8dd9db122106156616140c6fd805f28aa7d31
salah <salah@192.168.1.67>
2024-10-09 12:28:04 +0100

    fix : warning errors

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,10 +7,11 @@ import "react-native-reanimated";
 import { ClerkProvider, ClerkLoaded } from "@clerk/clerk-expo";
 import { tokenCache } from "@/lib/auth";
 import "react-native-get-random-values";
+import { LogBox } from "react-native";
 const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
-
+LogBox.ignoreLogs(["Clerk:"]);
 export default function RootLayout() {
   const [loaded] = useFonts({
     "Jakarta-Bold": require("../assets/fonts/PlusJakartaSans-Bold.ttf"),
